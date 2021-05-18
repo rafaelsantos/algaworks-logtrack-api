1652b2ef6fae47954253a7c152d70e2a61e69ebf
Rafael Santos <santos.rafaelbs@gmail.com>
2021-05-17 21:47:46 -0300

    Creating API with Spring Boot

---
+++ src/main/java/com/github/rafaelsantos/logtrack/AlgaworksLogtrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.rafaelsantos.logtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaworksLogtrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaworksLogtrackApiApplication.class, args);
+	}
+
+}
