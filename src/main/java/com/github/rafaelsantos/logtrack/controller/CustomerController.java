628b4e48b344c94f643c1c9995d97cbddc6b33ba
Rafael Santos <santos.rafaelbs@gmail.com>
2021-05-20 15:38:27 -0300

    Configuring jakarta persistence

--- src/main/java/com/github/rafaelsantos/logtrack/controller/CustomerController.java
+++ src/main/java/com/github/rafaelsantos/logtrack/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package com.github.rafaelsantos.logtrack.controller;
 
-import java.util.Arrays;
 import java.util.Collection;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +13,11 @@ import com.github.rafaelsantos.logtrack.model.Customer;
 @RestController
 public class CustomerController {
 	
+	@PersistenceContext
+	private EntityManager em;
+	
 	@GetMapping("/customers")
 	public Collection<Customer> findAll() {
-		//Using var: java11
-		var customer = new Customer(1L, "Johnny", "johnny.laurence@gmail.com", "45 9999-9999");
-		var customer2 = new Customer(2L, "Gary", "gary.byrnes@gmail.com", "75 5678-9999");
-		
-		return Arrays.asList(customer, customer2);
+		return em.createQuery("from Customer", Customer.class).getResultList();
 	}
 }
--- src/main/java/com/github/rafaelsantos/logtrack/model/Customer.java
+++ src/main/java/com/github/rafaelsantos/logtrack/model/Customer.java
@@ -1,12 +1,25 @@
 package com.github.rafaelsantos.logtrack.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "customer")
 public class Customer {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
 	private String name;
