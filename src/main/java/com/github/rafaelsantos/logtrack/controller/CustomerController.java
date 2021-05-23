0044332c1fc7d42322b4cf352e9e94bdb3727d9b
Rafael Santos <santos.rafaelbs@gmail.com>
2021-05-23 10:29:48 -0300

    Implementing DeleteMapping

--- src/main/java/com/github/rafaelsantos/logtrack/controller/CustomerController.java
+++ src/main/java/com/github/rafaelsantos/logtrack/controller/CustomerController.java
@@ -3,9 +3,11 @@ package com.github.rafaelsantos.logtrack.controller;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,5 +52,14 @@ public class CustomerController {
 		
 		customer.setId(id);
 		return ResponseEntity.ok(customerRepository.save(customer));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> remove(@PathVariable Long id) {
+		if (!customerRepository.existsById(id))
+			return ResponseEntity.notFound().build();
+		
+		customerRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
