dcd03627e4335a9562cece61837e3ea675f8b273
Rafael Santos <santos.rafaelbs@gmail.com>
2021-05-23 10:42:43 -0300

    Configuring Jakarta Bean Validation

--- src/main/java/com/github/rafaelsantos/logtrack/controller/CustomerController.java
+++ src/main/java/com/github/rafaelsantos/logtrack/controller/CustomerController.java
@@ -2,6 +2,8 @@ package com.github.rafaelsantos.logtrack.controller;
 
 import java.util.Collection;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
@@ -41,12 +43,12 @@ public class CustomerController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Customer save(@RequestBody Customer customer) {
+	public Customer save(@Valid @RequestBody Customer customer) {
 		return customerRepository.save(customer);
 	}
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<Customer> update(@PathVariable Long id, @RequestBody Customer customer) {
+	public ResponseEntity<Customer> update(@Valid @PathVariable Long id, @RequestBody Customer customer) {
 		if (!customerRepository.existsById(id))
 			return ResponseEntity.notFound().build();
 		
