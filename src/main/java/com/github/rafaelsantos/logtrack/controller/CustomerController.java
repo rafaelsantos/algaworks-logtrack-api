384c9f65ef0c1e81076aee7dccf66ac3bee893e2
Rafael Santos <santos.rafaelbs@gmail.com>
2021-05-23 09:58:45 -0300

    Adding pathVariable for API Rest route

--- src/main/java/com/github/rafaelsantos/logtrack/controller/CustomerController.java
+++ src/main/java/com/github/rafaelsantos/logtrack/controller/CustomerController.java
@@ -1,35 +1,34 @@
 package com.github.rafaelsantos.logtrack.controller;
 
 import java.util.Collection;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.rafaelsantos.logtrack.domain.model.Customer;
 import com.github.rafaelsantos.logtrack.domain.repository.CustomerRepository;
 
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
 	
 	@Autowired
 	private CustomerRepository customerRepository;
 	
-	@GetMapping("/customers")
+	@GetMapping
 	public Collection<Customer> findAll() {
 		return customerRepository.findAll();
 	}
 	
-	@GetMapping("/customers/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Customer> find(@PathVariable Long id) {
-		Optional<Customer> customer = customerRepository.findById(id);
-		
-		if(customer.isPresent())
-			return ResponseEntity.ok(customer.get());
-		
-		return ResponseEntity.notFound().build();
+		return customerRepository.findById(id)
+				.map(customer -> ResponseEntity.ok(customer))
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
